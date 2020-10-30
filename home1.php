d0381a30e49700d3e3a726d39835424c7abd7bf8
PragatiRaj <51054282+PragatiRaj@users.noreply.github.com>
2020-10-30 16:29:08 +0530

    This is home page file

---
+++ home1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url('homeback.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
+</style>
+</head>
+<body>
+<center>
+
+<h1>Welcome to Pragati Online Job Portal</h1>
+<h2>Find Your Job Based-On Your Skills!</h2>
+</center>
+</body>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 25px;
+ 
+}
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+
+<div class="topnav">
+<a class="active" href="#home">Home</a>
+  <a href="login.php">Login</a>
+  <a href="register.php">Register</a>
+</div>
+
+</body>
+
+</html>
+
